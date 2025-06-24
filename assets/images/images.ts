29342307de1dcb6d6fb5709c7467feee85c6e768
Felipe <felipezundlercontato@gmail.com>
2025-06-23 21:12:30 -0300

    imagens cards

--- assets/images/images.ts
+++ assets/images/images.ts
@@ -1,8 +1,8 @@
 export const imagens = {
     background: require("./background.jpg"),
     logo: require("./logo.png"),
-    guitarra: require("./fundo2.jpg"),
+    guitarra: require("./fundo2.png"),
     teclado: require("./acess.png"),
-    violao: require("./registraproduto.jpg"),
+    violao: require("./registraprodutos.jpg"),
   };
   
